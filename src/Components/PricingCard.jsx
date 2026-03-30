9b092dcee2e4e504b4ebab0cab7ddeeba965f750
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-03-30 23:07:20 +0600

    pricing card done

--- src/Components/PricingCard.jsx
+++ src/Components/PricingCard.jsx
@@ -1,28 +1,52 @@
 import React from 'react';
 
 const PricingCard = () => {
-    const cards= [
-        {title: 'Starter', des: 'Perfect for getting started', price: '$0', pricE: '/Month', li1: 'Access to 10 free tools', li2: 'Basic template', li3:'Community support',li4:'1 Project per month',btn:'Get Started Free' },
-        {title: 'Pro', des: 'Best for Professionals', price: '$29', pricE: '/Month', li1: 'Access to all premium tools', li2: 'Unlimited template', li3:'Priority support',li4:'Unlimited month',btn:'Start Pro Trial' },
-        {title: 'Enterprise', des: 'For teams and business', price: '$99', pricE: '/Month', li1: 'Everything in Pro', li2: 'Team collaboration', li3:'Custom integration',li4:'Dedicated Support', li5:'SLA gurantee',li6: 'Custom branding', btn:'Get Started Free' },
-    ]
     // console.log(cards);
     return (
         <div className='grid grid-cols-3  gap-[30px] mt-[40px]'>
-            {cards.map((card,i)=>(
-                <div key={i} className='border w-[340px]'>
-                    <h2>{card.title}</h2>
-                    <p>{card.des}</p>
-                    <p>{card.price}{card.pricE}</p>
-                    <ul>
-                        <li>✔{card.li1}</li>
-                        <li>✔{card.li2}</li>
-                        <li>✔{card.li3}</li>
-                        <li>✔{card.li4}</li>
-                    </ul>
-                    <button className='btn btn-success'>{card.btn}</button>
+            <div className='border shadow-xl border-zinc-200 p-4 w-[340px] rounded-lg'>
+                <p className='font-bold text-3xl'>Starter</p>
+                <p className='text-gray-500 mb-[24px]'>Perfect for getting started</p>
+                <p><span className='font-bold text-3xl'>$0</span><span className='text-gray-500'>/Month</span></p>
+                <ul className='mt-[24px]'>
+                    <li className='text-gray-500'>✔Access to free tools</li>
+                    <li className='text-gray-500'>✔Basic Templates</li>
+                    <li className='text-gray-500'>✔Community Support</li>
+                    <li className='text-gray-500'>✔1 Project per month</li>
+                </ul>
+                <button className='text-white rounded-2xl mt-[104px] w-full btn bg-linear-to-r from-[#662df7] to-[#871bfa]'>Get Started Free</button>
+            </div>
+            <div className='border text-white bg-linear-to-r from-[#662df7] to-[#871bfa] shadow-xl border-zinc-200 p-4 w-[380px] rounded-lg'>
+                <div className='flex justify-between items-center'>
+                    <p className='font-bold text-3xl'>Pro</p>
+                    <p className='bg-[#fff4c7] font-semibold p-1 rounded-full  text-[#db9f63]'>Most Popular</p>
                 </div>
-            ))}
+                <p className=' mb-[24px]'>Best for Professionals</p>
+                <p><span className='font-bold text-3xl'>$29</span><span>/Month</span></p>
+                <ul className='mt-[24px]'>
+                    <li>✔Access to all premium tools</li>
+                    <li>Unlimited Templates</li>
+                    <li>✔Priority Support</li>
+                    <li>✔Unlimited Project</li>
+                    <li>✔Cloud Sync</li>
+                    <li>✔Advanced analytics</li>
+                </ul>
+                <button className=' rounded-2xl mt-[64px] w-full btn bg-white text-[#871bfa]'>Start Pro Trial</button>
+            </div>
+            <div className='border shadow-xl border-zinc-200 p-4 w-[340px] rounded-lg'>
+                <p className='font-bold text-3xl'>Enterprise</p>
+                <p className='text-gray-500 mb-[24px]'>For teams and business</p>
+                <p><span className='font-bold text-3xl'>$99</span><span className='text-gray-500'>/Month</span></p>
+                <ul className='mt-[24px]'>
+                    <li className='text-gray-500'>✔Everything in Pro</li>
+                    <li className='text-gray-500'>✔Team Collaboration</li>
+                    <li className='text-gray-500'>✔Custom Integration</li>
+                    <li className='text-gray-500'>✔Dedicated Support</li>
+                    <li className='text-gray-500'>✔SLA Guarantee</li>
+                    <li className='text-gray-500'>✔Custom Branding</li>
+                </ul>
+                <button className='text-white rounded-2xl mt-[64px] w-full btn bg-linear-to-r from-[#662df7] to-[#871bfa]'>Contact Sales</button>
+            </div>
         </div>
     );
 };
